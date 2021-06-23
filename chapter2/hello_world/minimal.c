301996ff43d8564017ef7cae58dec4f6119ab4e9
yangxingwu <xingwu.yang@gmail.com>
2021-06-23 12:22:48 +0000

    load bpf code successfully

--- chapter2/hello_world/minimal.c
+++ chapter2/hello_world/minimal.c
@@ -1,14 +1,41 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "minimal.skel.h"
 
 int main(int argc, char **argv)
 {
-    if (load_bpf_file("bpf_program.o") != 0) {
-        printf("The kernel didn't load the BPF program\n");
-        return -1;
+	struct minimal_bpf *skel = NULL;
+	int err;
+
+	/* Open BPF application */
+	skel = minimal_bpf__open();
+	if (!skel) {
+		fprintf(stderr, "Failed to open BPF skeleton\n");
+		return 1;
+	}
+
+	/* Load & verify BPF programs */
+	err = minimal_bpf__load(skel);
+	if (err) {
+		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
+		goto cleanup;
+	}
+
+	/* Attach tracepoint handler */
+	err = minimal_bpf__attach(skel);
+	if (err) {
+		fprintf(stderr, "Failed to attach BPF skeleton\n");
+		goto cleanup;
+	}
+
+	printf("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` "
+	       "to see output of the BPF programs.\n");
+
+    while (1) {
+        sleep(10);
     }
 
-    read_trace_pipe();
-
-    return 0;
+cleanup:
+	minimal_bpf__destroy(skel);
+	return -err;
 }
